6535bc60c6450cd474ec3e59b10a91beffe9cc2c
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2022-05-03 21:56:30 +0700

    fix(.circleci/config.yml): add commit lint

--- test/test-yaml.js
+++
@@ -1,9 +0,0 @@
-const yamlLint = require('yaml-lint');
-
-yamlLint.lint('../.circleci/config.yml')
-    .then(() => {
-        console.log('Valid YAML file.');
-    })
-    .catch((error) => {
-        console.error('Invalid YAML file.', error);
-    });
