22be082e8abbaa912b7557940013954f75db9127
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-05-25 10:11:56 +0700

    test(docker.yml & test.js) and remove *

--- test/test.js
+++ test/test.js
@@ -12,11 +12,6 @@ if (result2 != 9) {
     throw new Error(errMsg);
 }
 
-let result4 = add(7, 9);
-if (result4 != 16) {
-    throw new Error(errMsg);
-}
-
 var result3 = add(6, 7);
 if (result3 != 13) {
     throw new Error(errMsg);
